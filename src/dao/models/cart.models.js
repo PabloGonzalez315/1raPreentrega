e1f63a96983426a68134ddab4229e1b6d6cb31f6
pablo <pablo.alejandro315@gmail.com>
2023-01-30 20:57:03 -0300

    practica integradora

---
+++ src/dao/models/cart.models.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const cartCollection = "carts";
+
+const cartSchema = new mongoose.Schema({
+    products: [
+        {
+            productId: String,
+            quantity: Number,
+        },
+    ],
+});
+
+export const cartModel = mongoose.model(cartCollection, cartSchema);
